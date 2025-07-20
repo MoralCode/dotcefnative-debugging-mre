f81adb2cdbe0ae428b7c47bf0d2d0f782f584dd3
Adrian Edwards <adrian@adriancedwards.com>
2025-07-20 16:19:58 -0400

    import dotcefand fixes to make it work

--- Program.cs
+++ Program.cs
@@ -1,24 +1,32 @@
-﻿class Program
+using DotCef;
+
+
+class Program
 {
 	
 	static async Task Main(string[] args)
 	{
 		using var cef = new DotCefProcess();
-		cef.OutputDataReceived += (msg) =>
-		{
-			if (msg == null)
-				return;
-			Console.WriteLine(msg);
-		};
-		cef.ErrorDataReceived += (msg) =>
-		{
-			if (msg == null)
-				return;
-			Console.WriteLine(msg);
-		};
+		// cef.OutputDataReceived += (msg) =>
+		// {
+		// 	if (msg == null)
+		// 		return;
+		// 	Console.WriteLine(msg);
+		// };
+		// cef.ErrorDataReceived += (msg) =>
+		// {
+		// 	if (msg == null)
+		// 		return;
+		// 	Console.WriteLine(msg);
+		// };
 		cef.Start("--disable-web-security");
-		var window = await cef.CreateWindowAsync("https://fast.com", (int)(1000), (int)(1000), (int)(1000), (int)(1000), title: title, developerToolsEnabled: true, proxyRequests: false, modifyRequests: false, modifyRequestBody: false, logConsole: true, resizable: true);
-		await window.LoadUrlAsync(targetUrl);
+		var window = await cef.CreateWindowAsync(
+			"https://fast.com",
+			(int)(1000),
+			(int)(1000),
+			(int)(1000),
+			(int)(1000), title: "test", developerToolsEnabled: true, proxyRequests: false, modifyRequests: false, modifyRequestBody: false, logConsole: true, resizable: true);
+		await window.LoadUrlAsync("https://www.google.com");
 		await window.WaitForExitAsync();
 	}
 
