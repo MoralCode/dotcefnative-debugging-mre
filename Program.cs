66459f48b7165ea1ba10b92b7a4685f8ab74be80
Adrian Edwards <adrian@adriancedwards.com>
2025-07-20 15:51:03 -0400

    setup from Koen

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿private static async Task Main(string[] args)
+{
+	using var cef = new DotCefProcess();
+	cef.OutputDataReceived += (msg) =>
+	{
+		if (msg == null)
+			return;
+		Console.WriteLine(msg);
+	};
+	cef.ErrorDataReceived += (msg) =>
+	{
+		if (msg == null)
+			return;
+		Console.WriteLine(msg);
+	};
+	cef.Start("--disable-web-security");
+	var window = await cef.CreateWindowAsync("https://fast.com", (int)(1000), (int)(1000), (int)(1000), (int)(1000), title: title, developerToolsEnabled: true, proxyRequests: false, modifyRequests: false, modifyRequestBody: false, logConsole: true, resizable: true);
+	await window.LoadUrlAsync(targetUrl);
+	await window.WaitForExitAsync();
+}
