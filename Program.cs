0f997389c2e66e1a84f9cb0dad90ef534a262410
Adrian Edwards <adrian@adriancedwards.com>
2025-07-20 18:37:01 -0400

    allow overriding the default Url via CLI

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-using DotCef;
+﻿using DotCef;
 
 
 class Program
@@ -6,6 +6,24 @@ class Program
 	
 	static async Task Main(string[] args)
 	{
+
+		string defaultUrl = "https://fast.com";
+		string targetUrl = defaultUrl;
+		List<string> cefArgs = new();
+
+		foreach (var arg in args)
+		{
+			if (arg.StartsWith("--url="))
+			{
+				targetUrl = arg.Substring("--url=".Length);
+			}
+			else
+			{
+				cefArgs.Add(arg);
+			}
+		}
+
+
 		using var cef = new DotCefProcess();
 		// cef.OutputDataReceived += (msg) =>
 		// {
@@ -19,11 +37,10 @@ class Program
 		// 		return;
 		// 	Console.WriteLine(msg);
 		// };
-		string extraArgs = string.Join(" ", args);
+		string extraArgs = string.Join(" ", cefArgs);
 
 		cef.Start($"--disable-web-security --ozone-platform-hint=wayland --enable-features=WaylandWindowDecorations --use-alloy-style --user-data-dir=./chromium-data-dir --log-file=cef.log --no-sandbox {extraArgs}");
 
-		var targetUrl = "http://google.com";
 		var window = await cef.CreateWindowAsync(
 			targetUrl,
 			(int)(1000),
